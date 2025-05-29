34313b0ab1308b18793c9e93f682c716d09d4b4d
Sachin Kumar <sachinrajputlive@gmail.com>
2025-05-29 13:43:18 +0530

    [main] vite config file fixes for production deployment also added comments for better clarification

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // Set base path for GitHub Pages deployment
   base: '/ice-wrap-task/',
   plugins: [react()],
+
   build: {
+    // Uncomment the following block to build as a library
+    /*
     lib: {
-      entry: "./src/index.jsx",
+      entry: './src/index.jsx',
       name: 'SachinDialogModal',
       fileName: (format) => `sachin-dialog-modal.${format}.js`,
     },
@@ -15,12 +19,19 @@ export default defineConfig({
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    */
+
+    // This part is used for app deployment
+    outDir: 'dist',
+    emptyOutDir: true,
   },
+
+  // Only needed if you're importing the library in a consumer app
   optimizeDeps: {
-    include: ['sachin-dialog-modal']
-  }
+    include: ['sachin-dialog-modal'],
+  },
 });
