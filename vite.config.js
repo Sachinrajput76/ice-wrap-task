201073339628631f191be866a19a0457eb765bb9
Sachin Kumar <sachinrajputlive@gmail.com>
2025-05-29 13:08:58 +0530

    [main] updated vite config, build key updated

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,24 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/ice-wrap-task/',
   plugins: [react()],
-  // build: {
-  //   lib: {
-  //     entry: "./src/index.jsx",
-  //     name: 'SachinDialogModal',
-  //     fileName: (format) => `sachin-dialog-modal.${format}.js`,
-  //   },
-  //   rollupOptions: {
-  //     external: ['react', 'react-dom'],
-  //     output: {
-  //       globals: {
-  //         react: 'React',
-  //         'react-dom': 'ReactDOM'
-  //       }
-  //     }
-  //   }
-  // },
+  build: {
+    // lib: {
+    //   entry: "./src/index.jsx",
+    //   name: 'SachinDialogModal',
+    //   fileName: (format) => `sachin-dialog-modal.${format}.js`,
+    // },
+    // rollupOptions: {
+    //   external: ['react', 'react-dom'],
+    //   output: {
+    //     globals: {
+    //       react: 'React',
+    //       'react-dom': 'ReactDOM'
+    //     }
+    //   }
+    // },
+    outDir: 'dist', // This is where the final site will be generated
+    emptyOutDir: true,
+  },
   optimizeDeps: {
     include: ['sachin-dialog-modal']
   }
