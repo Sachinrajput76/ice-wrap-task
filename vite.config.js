e4918c73dabd6abd11fa2eeacc20e643c0b2f56b
Sachin Kumar <sachinrajputlive@gmail.com>
2025-05-29 13:20:25 +0530

    [main] vite config file fixes

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,20 @@ export default defineConfig({
   base: '/ice-wrap-task/',
   plugins: [react()],
   build: {
-    // lib: {
-    //   entry: "./src/index.jsx",
-    //   name: 'SachinDialogModal',
-    //   fileName: (format) => `sachin-dialog-modal.${format}.js`,
-    // },
-    // rollupOptions: {
-    //   external: ['react', 'react-dom'],
-    //   output: {
-    //     globals: {
-    //       react: 'React',
-    //       'react-dom': 'ReactDOM'
-    //     }
-    //   }
-    // },
-    outDir: 'dist', // This is where the final site will be generated
-    emptyOutDir: true,
+    lib: {
+      entry: "./src/index.jsx",
+      name: 'SachinDialogModal',
+      fileName: (format) => `sachin-dialog-modal.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
   },
   optimizeDeps: {
     include: ['sachin-dialog-modal']
